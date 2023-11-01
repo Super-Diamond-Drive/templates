b68bd65cc518007f2a086995ed38a73ed49373c6
Alex <142623358+Alexidians@users.noreply.github.com>
2023-11-01 20:39:26 +0200

    Create last-number.js

---
+++ last-number.js
@@ -0,0 +1,20 @@
+// does + i
+
+var lastNumber = 0
+for (let i = 0; i < i + 1; i++) {
+ if(lastNumber + i == Infinity) {
+  console.log(lastNumber + i)
+  break;
+ }
+}
+
+//does + 1
+var lastNumber = 0
+for (let i = 0; i < cars.i + 1; i++) {
+ if(lastNumber + 1 == Infinity) {
+  console.log(lastNumber)
+ }
+ else {
+  lastNumber = lastNumber + 1
+ }
+}
